11721e73664c49a7c9e757bd9be3d94babfe36aa
DESKTOP-CMFB67U\GA2B <h1202315014@hiratagakuen.onmicrosoft.com>
2024-10-11 12:31:00 +0900

    Animation

--- 個人制作/Assets/Script/PlayerController.cs
+++ 個人制作/Assets/Script/PlayerController.cs
@@ -5,30 +5,35 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     Animator animator;
+    
+    Quaternion targetRotation;
 
     void Awake()
     {
         //コンポーネント関連付け
         TryGetComponent(out animator);
-    }
 
+        //初期化
+        targetRotation = transform.rotation;
+    }
     // Update is called once per frame
     void Update()
     {
         //入力ベクトルの取得
         var horizontal = Input.GetAxis("Horizontal");
-        var vertical = Input.GetAxis("Vertical");
-        var velocity = new Vector3(horizontal, 0, vertical).normalized;
-        var speed = Input.GetKey(KeyCode.LeftShift) ? 1 : 0.5f;
+        var vertical   = Input.GetAxis("Vertical");
+        var velocity   = new Vector3(horizontal, 0, vertical).normalized;
+        var speed      = Input.GetKey(KeyCode.LeftShift) ? 2 : 1;
+        var rotationSpeed = 600 * Time.deltaTime;
 
         //移動方向を向く
-        if(velocity.magnitude>0.5f)
+        if (velocity.magnitude>0.5f)
         {
-            transform.rotation = Quaternion.LookRotation(velocity, Vector3.up);
+            targetRotation = Quaternion.LookRotation(velocity, Vector3.up);
         }
+        transform.rotation = Quaternion.RotateTowards(transform.rotation, targetRotation, rotationSpeed);
 
         //移動速度をAnimatorに反映
         animator.SetFloat("Speed", velocity.magnitude * speed, 0.1f, Time.deltaTime);
-
     }
 }
